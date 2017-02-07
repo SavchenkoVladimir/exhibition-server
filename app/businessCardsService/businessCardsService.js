bd1e016523855b766378c1db7030d3b428bacb65
Vadimir <peers.rom@gmail.com>
2017-02-07 17:58:40 +0200

    Cleaned BusinessCardData is saved into DB.

--- app/businessCardsService/businessCardsService.js
+++ app/businessCardsService/businessCardsService.js
@@ -20,10 +20,6 @@ class BusinessCardsService {
         var getImageProcessingStatus = this.getImageProcessingStatus();
 
         curl.on('end', function (statusCode, responceBody, headers) {
-            console.log(statusCode);
-//            console.log(responceBody);
-//            console.info(headers);
-            console.log(this.getInfo('TOTAL_TIME'));
 
             if (responceBody) {
                 getImageProcessingStatus(responceBody);
@@ -41,49 +37,10 @@ class BusinessCardsService {
     }
 
     makeRequest() {
-        var curl = new Curl();
 
-        curl.setOpt(Curl.option.URL, 'http://cloud.ocrsdk.com/processBusinessCard?language=Russian&exportformat=xml');//vCard
-        curl.setOpt(Curl.option.USERNAME, config.abbyyAppName);
-        curl.setOpt(Curl.option.PASSWORD, config.abbyyPwd);
-        curl.setOpt(Curl.option.HTTPPOST, [
-//            {name: 'business-card', file: `${imagesDir + imageName}`, type: 'image/jpeg'},
-            {name: 'my_file', file: `${imagesDir}/1486372547940.jpeg`, type: 'image/jpeg'}
-        ]);
-
-        var getImageProcessingStatus = this.getImageProcessingStatus();
-
-        curl.on('end', function (statusCode, responceBody, headers) {
-            console.log(statusCode);
-//            console.log(responceBody);
-//            console.info(headers);
-            console.log(this.getInfo('TOTAL_TIME'));
-
-            if (responceBody) {
-                getImageProcessingStatus(responceBody);
-            }
-
-            this.close();
-        });
-
-        curl.on('error', function (err) {
-            console.log(err);
-            this.close();
-        });
-
-        curl.perform();
     }
 
     getImageProcessingStatus() {
-
-        // Body responce example
-        /*
-         <?xml version="1.0" encoding="utf-8"?>
-         <response>
-         <task id="b12b87c1-bc6d-4264-9710-98ecec404d44" registrationTime="2017-02-06T17:05:23Z" statusChangeTime="2017-02-06T17:05:23Z" status="Queued" filesCount="1" credits="10" estimatedProcessingTime="5" />
-         </response>                  
-         */
-
         var getProcessedImageLink = this.getProcessedImageLink();
 
         return function (responceBody) {
@@ -101,15 +58,6 @@ class BusinessCardsService {
     }
 
     getProcessedImageLink() {
-
-        // Body responce example
-        /*
-         <?xml version="1.0" encoding="utf-8"?>
-         <response>
-         <task id="b12b87c1-bc6d-4264-9710-98ecec404d44" registrationTime="2017-02-06T17:05:23Z" statusChangeTime="2017-02-06T17:05:25Z" status="Completed" filesCount="1" credits="10" resultUrl="https://ocrsdk.blob.core.windows.net/files/b12b87c1-bc6d-4264-9710-98ecec404d44.result?sv=2012-02-12&se=2017-02-07T03%3A00%3A00Z&sr=b&si=downloadResults&sig=ycfc57PiaUsNB0KG4E%2BVXJcrOjqb5kyne%2BaX02Xzm0k%3D"/>
-         </response>
-         */
-
         var deriveImageLink = this.deriveImageLink();
         var getProcessedData = this.getProcessedData();
 
@@ -122,16 +70,9 @@ class BusinessCardsService {
 
             curl.on('end', function (statusCode, responceBody, headers) {
 
-                console.log(statusCode);
-//                console.log(responceBody);
-//                console.log(headers);
-                console.log(this.getInfo('TOTAL_TIME'));
-
                 // TODO: implement flow dependancy response 'status'
                 if (responceBody) {
                     let imageLink = deriveImageLink(responceBody);
-
-                    console.log(imageLink);
 
                     getProcessedData(imageLink);
                 }
@@ -176,20 +117,12 @@ class BusinessCardsService {
 
             curl.on('end', function (statusCode, responceBody, headers) {
 
-//                console.log(statusCode);
-                console.log(responceBody);
-//                console.log(headers);
-                console.log(this.getInfo('TOTAL_TIME'));
-
                 if (responceBody) {
                     const parsedResponce = parseXML(responceBody);
-                    const processedImageData = parsedResponce.root.children[0];
+                    const processedImageData = parsedResponce.root.children[0].children;
                     const cleanedProcessedData = cleanProcessedData(processedImageData);
 
                     saveProcessedData(cleanedProcessedData);
-
-//                    console.log(cleanedProcessedData);
-//                    console.log(JSON.stringify(cleanedProcessedData));
                 }
 
                 this.close();
@@ -204,20 +137,36 @@ class BusinessCardsService {
         };
     }
 
-    // TODO: implement the method
     cleanProcessedData() {
 
         return function (rawProcessedData) {
-            return rawProcessedData;
+            let cleanedProcessedData = [];
+
+            for (let row = 0; row < rawProcessedData.length; row++) {
+                let field = {};
+
+                if (typeof rawProcessedData[i].attributes !== undefined) {
+                    field.fieldType = rawProcessedData[i].attributes.type;
+                }
+
+                if (typeof rawProcessedData[i].children !== undefined) {
+                    field.fieldValue = rawProcessedData[i].children[0].content;
+                }
+
+                cleanedProcessedData.push(field);
+            }
+
+            return cleanedProcessedData;
         };
     }
 
+    // TODO: implement the poleId
     saveProcessedData() {
 
         return function (processedData) {
             var buisnessCardDataModel = new BuisnessCardDataModel({
                 cardData: processedData,
-                poleId: 'Not yet implemented.'
+                poleId: 'Feature does not yet implemented.'
             });
 
             buisnessCardDataModel.save(function (err) {
