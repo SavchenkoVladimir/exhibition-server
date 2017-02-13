5280deebac4035891eef1bd40e9ad4f5724c8be1
Vadimir <peers.rom@gmail.com>
2017-02-13 14:00:35 +0200

    MS BusinessCartService is done.

--- app/businessCardsService/MSService.js
+++ app/businessCardsService/MSService.js
@@ -1,70 +1,95 @@
 var Curl = require('node-libcurl').Curl;
 var config = require('../config/config');
 var BuisnessCardDataModel = require('../models/BuisnessCardData');
+var AYLIENTextAPI = require('aylien_textapi');
 
 class MSService {
 
     serviceRun(imageName) {
-        var curl = new Curl();
-        const imagesDir = `${__dirname}/../public/uploads`;
-
-        var cleanProcessedData = this.cleanProcessedData();
-        var saveProcessedData = this.saveProcessedData();
-
-        curl.setOpt(Curl.option.URL, 'https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr?language=de');
-        curl.setOpt('HTTPHEADER', [`Ocp-Apim-Subscription-Key:${config.MSAPIKey}`]);
-        curl.setOpt(Curl.option.HTTPPOST, [
-            {name: 'image', file: `${imagesDir}/${imageName}`}
-        ]);
-
-        curl.on('end', function (statusCode, responceBody, headers) {
-            console.log(statusCode);
-
-            if (responceBody) {
-                const cleanedProcessedData = cleanProcessedData(responceBody);
-//                saveProcessedData(cleanedProcessedData);                
-            }
-
-            this.close();
-        });
-
-        curl.on('error', function (err) {
-            this.close();
-        });
-
-        curl.perform();
+        const extractBusinessCartData = this.extractBusinessCartData();
+        extractBusinessCartData(imageName);
     }
 
-    cleanProcessedData() {
+    extractBusinessCartData() {
+        var curl = new Curl();
+        const imgDir = `${__dirname}/../public/uploads`;
 
-        return function (rawProcessedData) {
-            let cleanedProcessedData = '';
-            const responseData = JSON.parse(rawProcessedData);
+        let extractResponseText = this.extractResponseText();
+        let normalizeCartText = this.normalizeCartText();
 
-            for (let i = 0; i < responseData.regions.length; i++) {
-                let boundingBox = responseData.regions[i].lines;
+        return function (imageName) {
+            curl.setOpt(Curl.option.URL, 'https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr?language=de');
+            curl.setOpt('HTTPHEADER', [`Ocp-Apim-Subscription-Key:${config.MSAPIKey}`]);
+            curl.setOpt(Curl.option.HTTPPOST, [
+                {name: 'image', file: `${imgDir}/${imageName}`}
+            ]);
+
+            curl.on('end', function (statusCode, responseText, headers) {
+                if (responseText) {
+                    const businessCartText = extractResponseText(responseText);
+                    normalizeCartText(businessCartText);
+                }
+
+                this.close();
+            });
+
+            curl.on('error', function (err) {
+                this.close();
+            });
+
+            curl.perform();
+        };
+    }
+
+    extractResponseText() {
+
+        return function (rawBusinessCartText) {
+            let extractedBusinessCartText = '';
+            const businessCartText = JSON.parse(rawBusinessCartText);
+
+            for (let i = 0; i < businessCartText.regions.length; i++) {
+                let boundingBox = businessCartText.regions[i].lines;
 
                 for (let n = 0; n < boundingBox.length; n++) {
                     let words = boundingBox[n].words;
 
                     for (let k = 0; k < words.length; k++) {
                         const word = words[k];
-                        cleanedProcessedData += ` ${word.text}`;
+                        extractedBusinessCartText += ` ${word.text}`;
                     }
                 }
             }
+            
+            //Remove on prod
+            console.log(extractedBusinessCartText);
+            return extractedBusinessCartText;
+        };
+    }
 
-            console.log(cleanedProcessedData);
-            return cleanedProcessedData;
+    // Extracts from text its entities as Name, URL, Phone and so on
+    normalizeCartText() {
+        let saveBusinessCartData = this.saveBusinessCartData();
+
+        return function (cartText) {
+
+            var textApi = new AYLIENTextAPI({
+                application_id: config.aylienAppId,
+                application_key: config.aylienAPIKey
+            });
+            textApi.entities({'text': cartText}, function (error, response) {
+                if (error === null) {
+                    saveBusinessCartData(response);
+                }
+            });
         };
     }
 
     // TODO: implement the poleId
-    saveProcessedData() {
+    saveBusinessCartData() {
 
-        return function (processedData) {
+        return function (businessCartData) {
             var buisnessCardDataModel = new BuisnessCardDataModel({
-                cardData: processedData,
+                cardData: businessCartData,
                 poleId: 'Feature does not yet implemented.'
             });
 
--- app/config/config.js
+++ app/config/config.js
@@ -5,5 +5,8 @@ module.exports = {
     'abbyyPwd': '+U6AwRrtBC9sU4sNEib6qIWg',
     'FullContactAPIKey': '99801783e77fd666',
     'googleProjectID': '1026846243805',
-    'MSAPIKey': '0cc54bb443644573a1b75aceccdfeac7'
+    'MSAPIKey': '0cc54bb443644573a1b75aceccdfeac7', 
+    'aylienAppId': '8566588d',
+    'aylienAPIKey': '6b8cbdc4a2bdc99ada941b23e11b1db5'
+    
 };
